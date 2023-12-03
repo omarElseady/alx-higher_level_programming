fa67e8f783851bc732e4188f13631cf91525727c
Ibrahim_Adel <hima96326@gmail.com>
2023-12-03 15:21:49 -0800

    My second commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    sumo = len(sys.argv) - 1
+    if sumo == 0:
+        print("0 arguments.")
+    elif sumo == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(sumo))
+    for j in range(count):
+        print("{}: {}".format(j + 1, sys.argv[j + 1]))
