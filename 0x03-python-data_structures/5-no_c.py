6004427005aa2e18810908d1d2350845849263d4
Ibrahim_Adel <hima96326@gmail.com>
2023-12-04 12:43:42 -0800

    My second commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new = ""
     for i in range(len(my_string)):
-        if (my_string[i] == 'c' or my_string[i] =='C':
+        if (my_string[i] == 'c' or my_string[i] =='C'):
                 continue
         else:
                 new += mu_string[i]
