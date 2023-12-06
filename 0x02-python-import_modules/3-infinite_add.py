5c2bed1e71bc973cd8ffb8b29c68c986349bb976
Ibrahim_Adel <hima96326@gmail.com>
2023-12-06 12:26:45 -0800

    My second commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+
     import sys
 
     cou = 0
     for i in range(len(sys.argv) - 1):
         cou += int(sys.argv[i + 1])
-    print("{}".format(total))
+    print("{}".format(cou))
